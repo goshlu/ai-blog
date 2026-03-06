bf4d750a0f86aa4addf05d8ec016ef8d42076d20
wishplash <yangshiheclow@gmail.com>
2026-03-06 11:04:26 +0800

    Correct Post type export

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,5 +1,7 @@
 // 重新导出 Prisma 生成的类型，保持向后兼容
-export type { Post, Tag } from "@prisma/client";
+import type { Post, Tag } from "@prisma/client";
+
+export type { Post, Tag };
 
 // 带标签的 Post 类型
 export type PostWithTags = Post & {
